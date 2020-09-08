ebfcef5410155e46a1732c88f6ecfb2622286dc7
Signior-X <b19188@students.iitmandi.ac.in>
2020-09-08 07:03:26 +0530

    Updates

---
+++ precache-manifest.bb967a52db2a8969fe63a9f29f9fbe55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a9a1e6a9f7d6490564170adac0c7b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa1efd03aab8eaa2eaf9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f059ccace5a3d1f5d094",
+    "url": "/static/js/2.f7248e7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7248e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa1efd03aab8eaa2eaf9",
+    "url": "/static/js/main.2c4965a1.chunk.js"
+  },
+  {
+    "revision": "8da32427f6dff9b5abfa",
+    "url": "/static/js/runtime-main.99e80802.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
